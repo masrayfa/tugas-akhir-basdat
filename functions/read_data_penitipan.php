0ae7d907ebfb88bf76fda20ef887c3078e216709
DR34MBoy <87920291+DR34MBoy@users.noreply.github.com>
2021-12-01 19:35:07 +0700

    fix sedikit bagian fetch

--- functions/read_data_penitipan.php
+++ functions/read_data_penitipan.php
@@ -28,20 +28,19 @@
         <?php
         $data_result = pg_query("SELECT * FROM data_penitipan");
 
-        while($data_penitipan = pg_fetch_array($data_result)){
-            echo "<tr>";
-
-			echo "<td>".$data_penitipan['id']."</td>";
-			echo "<td>".$data_penitipan['tanggal_penerimaan']."</td>";
-			echo "<td>".$data_penitipan['tanggal_pengembalian']."</td>";
-			echo "<td>".$data_penitipan['biaya_penitipan']."</td>";
-			echo "<td>".$data_penitipan['id_hewan']."</td>";
-            echo "<td>".$data_penitipan['nik_pemilik']."</td>";
-			echo "<td>".$data_penitipan['id_petugas']."</td>";
-			           
-            echo "</tr>";
+        $no = 1;
+        while($data_penitipan = pg_fetch_array($data_result)){ ?>
+            <tr>
+                <td><?= $no++ ?></td>;
+                <td><?= $data_penitipan['tanggal_penerimaan']?></td>;
+                <td><?= $data_penitipan['tanggal_pengembalian']?></td>;
+                <td><?= $data_penitipan['biaya_penitipan']?></td>;
+                <td><?= $data_penitipan['id_hewan']?></td>;
+                <td><?= $data_penitipan['nik_pemilik']?></td>;
+                <td><?= $data_penitipan['id_petugas']?></td>;   
+            </tr>;
         }
-        ?>
+        <?php } ?>
     </tbody>
     </table>
 </body>
