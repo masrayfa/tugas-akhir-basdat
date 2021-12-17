f1f0ec30340fba3a5d8b073df288d8f2233d1d68
DR34MBoy <87920291+DR34MBoy@users.noreply.github.com>
2021-12-17 14:10:23 +0700

    Update read_data_penitipan.php

--- functions/read_data_penitipan.php
+++ functions/read_data_penitipan.php
@@ -11,30 +11,46 @@
 
 
     <nav>
-        <a href="create_page.php">[+] Daftar </a>
+        <a href="../pages/hewan_page.php">[+] Daftar </a>
     </nav>
 
     <br>
     <table border = "1">
     <thead>
         <tr>
-            <th id= 'no'>No</th>
-            <th id = 'tanggal_penerimaan'>Tanggal Penerimaan</th>
-            <th id = 'tanggal_pengembalian'>Tanggal Pengembalian</th>
-            <th id = 'biaya'>Biaya Penitipan</th>
-            <th id = 'id_hewan'>ID Hewan</th>
-            <th id = 'nik'>NIK Pemilik</th>
-            <th id = 'id_petugas'>ID Petugas</th>
-            <th id = 'tindakan'>Tindakan</th>
+            <th>No</th>
+            <th>Tanggal Penerimaan</th>
+            <th>Tanggal Pengembalian</th>
+            <th>Biaya Penitipan</th>
+            <th>ID Hewan</th>
+            <th>NIK Pemilik</th>
+            <th>ID Petugas</th>
+            <th>Tindakan</th>
         </tr>
     </thead>
     <tbody>
 
         <?php
-        $query = pg_query("SELECT * FROM data_penitipan");
+        $query = pg_query("SELECT waktu_penerimaan,waktu_pengembalian,biaya_penitipan,pemilik.id_hewan,nik_pemilik,id_petugas FROM pemilik,data_penitipan WHERE pemilik.nik=data_penitipan.nik_pemilik");
 
-        echo "PROSES!"
-        ?>
+        $no = 1;
+			while ($data = pg_fetch_array($query)) { ?>
+				<tr>
+					<td><?= $no++ ?></td>
+					<td><?= $data['waktu_penerimaan'] ?></td>
+					<td><?= $data['waktu_pengembalian'] ?></td>
+					<td><?= $data['biaya_penitipan'] ?></td>
+					<td><?= $data['id_hewan'] ?></td>
+					<td><?= $data['nik_pemilik'] ?></td>
+                    <td><?= $data['id_petugas'] ?></td>
+					<td>
+                        <a href="hapussiwa.php?id=<?= $siswa['id'] ?>">delete</a>
+						<a href="formedit.php?id=<?=$siswa['id']?>">edit</a>
+					</td
+					
+	
+				</tr>
+		<?php } ?>
     </tbody>
     </table>
 </body>
