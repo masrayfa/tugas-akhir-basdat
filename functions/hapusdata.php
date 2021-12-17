d04b0df54b4d2c6579163591ba1545d517a501dc
hanunsaid <hanunsaid@apps.ipb.ac.id>
2021-12-17 18:02:22 +0700

    hapus data

--- functions/hapusdata.php
+++ functions/hapusdata.php
@@ -6,5 +6,11 @@ if( isset($_GET['id']) ){
 
     $query = pg_query("DELETE FROM data_penitipan WHERE nik_pemilik::int=$id");
     $query2 = pg_query("DELETE FROM pemilik WHERE nik::int=$id");
+
+    if( $query && $query2){
+        header('Location: read_data_penitipan.php');
+    } else {
+        die("gagal menghapus...");
+    }
 }
 ?>
