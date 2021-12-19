eb8750e06a29791bf23feb1be06f4dcb2fe8894c
DR34MBoy <87920291+DR34MBoy@users.noreply.github.com>
2021-12-19 18:22:51 +0700

    Delete functions directory

--- functions/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = pg_connect('host=localhost dbname= penitipan_hewan user=postgres password=');
-if(!$db) {
-    die("Gagal terhubung ke database: ". pg_connect_error());
-} else {
-    echo "Terkoneksi ke database ";
-}
-?>
