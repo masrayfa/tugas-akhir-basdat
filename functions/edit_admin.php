8c19acc0ffe0d7cd0a66cd4df8b53e2c392c5c01
DR34MBoy <87920291+DR34MBoy@users.noreply.github.com>
2021-12-20 10:04:43 +0700

    Add files via upload

---
+++ functions/edit_admin.php
@@ -0,0 +1,29 @@
+<?php
+include "../functions/config.php";
+
+if(isset($_POST['edit'])) {
+  if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $waktu_penerimaan = $_POST['waktu_penerimaan'];
+    $waktu_pengembalian = $_POST['waktu_pengembalian'];
+    $biaya_penitipan = $_POST['biaya_penitipan'];
+    $id_petugas = $_POST['id_petugas'];  
+
+    
+    $admin_edit_query = "UPDATE data_penitipan SET waktu_penerimaan = '$waktu_penerimaan', waktu_pengembalian = '$waktu_pengembalian', biaya_penitipan = '$biaya_penitipan', id_petugas='$id_petugas' WHERE nik_pemilik = '$id'";
+    
+    $admin_edit = pg_query($db, $admin_edit_query);
+    
+    
+    if($admin_edit) {
+      header("Location: read_data_penitipan_admin.php?");
+    } else {
+      header("Location: ../pages/home.php?");  
+    }
+  }
+}
+else {
+  header("Location: ../pages/home.php?");
+}
+  
+?>
---
+++ functions/edit_pemilik.php
@@ -0,0 +1,35 @@
+<?php
+include "../functions/config.php";
+
+if(isset($_POST['edit'])) {
+  if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $nik = $_POST['nik'];
+    $nama = $_POST['nama'];
+    $no_telepon = $_POST['telepon'];
+    $alamat = $_POST['alamat'];
+    $email = $_POST['email'];
+    $waktu_penerimaan = $_POST['waktu_penerimaan'];
+
+    $data_penitipan_edit_query = "UPDATE data_penitipan SET waktu_penerimaan = '$waktu_penerimaan' WHERE nik_pemilik = '$id'";
+
+    $data_penitipan_edit = pg_query($db, $data_penitipan_edit_query);
+    
+    $pemilik_edit_query = "UPDATE pemilik SET nik = '$nik', nama = '$nama', no_telepon = '$no_telepon', alamat='$alamat', email='$email' WHERE nik = '$id'";
+    
+    $pemilik_edit = pg_query($db, $pemilik_edit_query);
+    
+    
+    
+    if($pemilik_edit && $data_penitipan_edit) {
+      header("Location: read_data_penitipan.php?");
+    } else {
+      header("Location: ../pages/home.php?");  
+    }
+  }
+}
+else {
+  header("Location: ../pages/home?");
+}
+  
+?>
