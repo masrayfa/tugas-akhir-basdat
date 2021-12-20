f027eacd7ea4c8a19f2fdaecec7b8e7b8f0df8f5
DR34MBoy <87920291+DR34MBoy@users.noreply.github.com>
2021-12-20 13:32:26 +0700

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -19,7 +19,7 @@
     <div class="container my-5">
     <h1>Selamat Datang di Form Penitipan Hewan</h1>
       <form method="post">
-        <button type="submit" name= "submit" class="btn btn-primary my-3"><a href="hewan_page.php" class='text-light'>Input Hewan</button>
+        <button type="submit" name= "submit" class="btn btn-primary my-3"><a href="hewan_page.php" class='text-light'>Pemilik & Hewan Peliharaan</button>
       </form>
       <form method="post">
         <button type="submit" name= "submit" class="btn btn-primary my-3"><a href="petugas_page.php" class='text-light'>Petugas</button>
