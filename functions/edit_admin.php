ee84dbf75afb4938caddc5780bb62f0e8aaa5862
DR34MBoy <87920291+DR34MBoy@users.noreply.github.com>
2021-12-20 09:57:03 +0700

    Update edit_admin.php

--- functions/edit_admin.php
+++ functions/edit_admin.php
@@ -5,12 +5,12 @@ if(isset($_POST['edit'])) {
   if(isset($_GET['id'])) {
     $id = $_GET['id'];
     $waktu_penerimaan = $_POST['waktu_penerimaan'];
-    $waktu_pengemmbalian = $_POST['waktu_pengembalian'];
+    $waktu_pengembalian = $_POST['waktu_pengembalian'];
     $biaya_penitipan = $_POST['biaya_penitipan'];
     $id_petugas = $_POST['id_petugas'];  
 
     
-    $admin_edit_query = "UPDATE data_penitipan SET waktu_penerimaan = '$waktu_penerimaan', waktu_pengembalian = '$waktu_pengemmbalian', biaya_penitipan = '$biaya_penitipan', id_petugas='$id_petugas' WHERE nik_pemilik = '$id'";
+    $admin_edit_query = "UPDATE data_penitipan SET waktu_penerimaan = '$waktu_penerimaan', waktu_pengembalian = '$waktu_pengembalian', biaya_penitipan = '$biaya_penitipan', id_petugas='$id_petugas' WHERE nik_pemilik = '$id'";
     
     $admin_edit = pg_query($db, $admin_edit_query);
     
